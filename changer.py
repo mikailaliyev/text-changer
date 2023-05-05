dc777dcd31f06090ba739d5e590acb07754bf80d
Mikayil Aliyev <Mikayil.Aliyev@azpetrol.com>
2023-05-05 09:28:40 +0400

    added: copy to clipboard option now is active

--- changer.py
+++ changer.py
@@ -1,7 +1,12 @@
 from tkinter import *
 import re
-def show():
-    print(input_text.get(1.0, "end-1c"))
+import pyperclip
+
+
+def copy_to_clipboard():
+    text = input_text.get(1.0, "end-1c")
+    pyperclip.copy(text)
+
 
 def upper():
     text = input_text.get(1.0, "end-1c")
@@ -9,51 +14,53 @@ def upper():
     text = text.upper()
     input_text.insert(1.0, text)
     deselect(upper_text_value)
-    
+
+
 def lower():
     text = input_text.get(1.0, "end-1c")
     input_text.delete('1.0', END)
     text = text.lower()
     input_text.insert(1.0, text)
     deselect(lower_text_value)
-        
+
+
 def capitalize():
     text = input_text.get(1.0, "end-1c")
     input_text.delete('1.0', END)
     deselect(capitalize_text_value)
-    
-    #if a sentence doesn't have any punctuation marks
+
+    # if a sentence doesn't have any punctuation marks
     if ".?!" not in text:
         text = text.capitalize()
-        input_text.insert(1.0, text)   
+        input_text.insert(1.0, text)
         return
-    
-    #finding indexes of punctuation marks
+
+    # finding indexes of punctuation marks
     dots_list = []
     for i in re.finditer(r'[.!?]', string=text):
         dots_list.append(i.start())
 
-    #splitting text into list items according to punctuation marks' indexes
+    # splitting text into list items according to punctuation marks' indexes
     temp_list = []
     first = 0
     for i in dots_list:
-        temp_list.append(text[first : i + 1])
+        temp_list.append(text[first: i + 1])
         first = i + 1
-        
+
     # capitalizing every first letter of the list items
     for i in range(len(temp_list)):
-        letter = re.findall("[A-z]", temp_list[i])[0]        
+        letter = re.findall("[A-z]", temp_list[i])[0]
         temp_list[i] = list(temp_list[i])
         temp_list[i][temp_list[i].index(letter)] = letter.capitalize()
         temp_list[i] = "".join(temp_list[i])
     text = "".join(temp_list)
-    
+
     # clearing lists
     dots_list = []
     temp_list = []
-    input_text.insert(1.0, text)    
-    
-    
+    input_text.insert(1.0, text)
+
+
 def capitalize_all():
     text = input_text.get(1.0, "end-1c")
     input_text.delete('1.0', END)
@@ -64,7 +71,8 @@ def capitalize_all():
         if "\n" in i:
             start = i.find("\n")
             end = i.rfind("\n")
-            new_list.append(i[0: start].capitalize() + i[start: end+1] + i[end + 1:].capitalize())
+            new_list.append(i[0: start].capitalize() +
+                            i[start: end+1] + i[end + 1:].capitalize())
         else:
             new_list.append(i.capitalize())
     print(new_list)
@@ -73,11 +81,16 @@ def capitalize_all():
     deselect(capitalize_all_text_value)
 
 # deselecting other buttons
+
+
 def deselect(current):
-    button_values_list = [upper_text_value, lower_text_value, capitalize_text_value, capitalize_all_text_value]
-    button_values_list.remove(current) #removing current button value from the list to allow it to stay active
+    button_values_list = [upper_text_value, lower_text_value,
+                          capitalize_text_value, capitalize_all_text_value]
+    # removing current button value from the list to allow it to stay active
+    button_values_list.remove(current)
     for i in button_values_list:
         i.set(False)
+
 
 window = Tk()
 window.title("Text Changer")
@@ -88,27 +101,31 @@ input_text.place(x=5, y=5, height=710, width=800)
 
 # BUTTONS
 
-#All text UPPERCASE
+# All text UPPERCASE
 upper_text_value = BooleanVar(value=False)
-upper_text = Checkbutton(text="Upper", variable=upper_text_value, command=upper)
+upper_text = Checkbutton(
+    text="Upper", variable=upper_text_value, command=upper)
 upper_text.place(x=810, y=5)
 
-#All text lowercase
+# All text lowercase
 lower_text_value = BooleanVar(value=False)
-lower_text = Checkbutton(text="Lower", variable = lower_text_value, command=lower)
+lower_text = Checkbutton(
+    text="Lower", variable=lower_text_value, command=lower)
 lower_text.place(x=900, y=5)
 
-#First string Capitalize
+# First string Capitalize
 capitalize_text_value = BooleanVar(value=False)
-capitalize_text = Checkbutton(text="Capitalize", variable = capitalize_text_value, command=capitalize)
+capitalize_text = Checkbutton(
+    text="Capitalize", variable=capitalize_text_value, command=capitalize)
 capitalize_text.place(x=1000, y=5)
 
-#All strings Capitalize
+# All strings Capitalize
 capitalize_all_text_value = BooleanVar(value=False)
-capitalize_all_text = Checkbutton(text="Capitalize All", variable = capitalize_all_text_value, command=capitalize_all)
+capitalize_all_text = Checkbutton(
+    text="Capitalize All", variable=capitalize_all_text_value, command=capitalize_all)
 capitalize_all_text.place(x=1100, y=5)
 
-#All text copied
-show_text = Button(text="Copy text", command=show)
+# All text copied
+show_text = Button(text="Copy text", command=copy_to_clipboard)
 show_text.place(x=810, y=690)
 window.mainloop()
